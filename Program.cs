5b69b465f92c21c738c54698555efcba83ebae6e
Dziarhun Mikalai <dergunnik@gmail.com>
2026-02-10 00:18:18 +0300

    table stat output

--- Program.cs
+++ Program.cs
@@ -34,6 +34,9 @@ foreach (var t in tokens)
 
 if (tokens.Count == 0) return;
 
+const string SeparatorLine = "====================";
+
+Console.WriteLine();
 Console.WriteLine();
 Console.WriteLine();
 Console.WriteLine("Stats");
@@ -43,22 +46,33 @@ Console.WriteLine();
 tokens.Sort((a, b) => a.Kind.CompareTo(b.Kind));
 var curKind =  tokens[0].Kind;
 List<Token> curList = [];
+
 foreach (var t in tokens)
 {
     if (curKind != t.Kind)
     {
-        var cntd = curList.CountBy(t => t.Value);
-        var i = 1;
-        Console.WriteLine(curKind.ToString());
-        foreach (var p in cntd)
-        {
-            Console.WriteLine($"{i++}\t{p.Value}\t{p.Key}");
-        }
-        Console.WriteLine();
+        PrintTokenTypeList();
         curList = [t];
         curKind = t.Kind;
     } else
     {
         curList.Add(t);
     }
+}
+PrintTokenTypeList();
+
+void PrintTokenTypeList()
+{
+    var cntd = curList.CountBy(t => t.Value);
+    var i = 1;
+    Console.WriteLine(curKind.ToString());
+    Console.WriteLine(SeparatorLine);
+    Console.WriteLine("i\tcnt\tval");
+    Console.WriteLine(SeparatorLine);
+    foreach (var p in cntd)
+    {
+        Console.WriteLine($"{i++}\t{p.Value}\t{p.Key}");
+    }
+    Console.WriteLine(SeparatorLine);
+    Console.WriteLine();
 }
