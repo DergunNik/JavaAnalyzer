1e177ef93a289f7dc54db0739534d618fb675948
Dziarhun Mikalai <dergunnik@gmail.com>
2026-02-10 15:53:33 +0300

    better literals output

--- Program.cs
+++ Program.cs
@@ -63,13 +63,21 @@ PrintTokenTypeList();
 
 void PrintTokenTypeList()
 {
-    var cntd = curList.CountBy(t => t.Value);
+    IEnumerable<KeyValuePair<string, int>> cntDict;
+    if (curKind == TokenKind.LITERAL)
+    {
+        cntDict = curList.CountBy(t => t.ToString()[7..]);
+    }
+    else
+    {
+        cntDict = curList.CountBy(t => t.Value);
+    }
     var i = 1;
     Console.WriteLine(curKind.ToString());
     Console.WriteLine(SeparatorLine);
     Console.WriteLine("i\tcnt\tval");
     Console.WriteLine(SeparatorLine);
-    foreach (var p in cntd)
+    foreach (var p in cntDict)
     {
         Console.WriteLine($"{i++}\t{p.Value}\t{p.Key}");
     }
